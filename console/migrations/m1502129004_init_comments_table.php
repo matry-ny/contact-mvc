bb22ac1ff48ad2d92b8d21bf95cdd67140af19ef
Dmytro Kotenko <d.kotenko@i.ua>
2017-08-07 21:08:55 +0300

    Added comments table migration

---
+++ console/migrations/m1502129004_init_comments_table.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace console\migrations;
+
+use components\Model;
+
+/**
+ * Class m1502129004_init_comments_table
+ * @package console\migrations;
+ */
+class m1502129004_init_comments_table extends Model {
+    public function up()
+    {
+        $this->createTable('comments', [
+            'id INT(11) NOT NULL AUTO_INCREMENT',
+            'user_name VARCHAR(255) NOT NULL',
+            'comment TEXT NOT NULL',
+            'created_at DATETIME',
+            'updated_at DATETIME',
+            'PRIMARY KEY(`id`)'
+        ]);
+    }
+    
+    public function down()
+    {
+    }
+}
