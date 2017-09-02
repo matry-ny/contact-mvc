d35e705fe33b37bab5b8a4417d0da79b10b61792
phpacad <git@php-academy.kiev.ua>
2017-09-02 13:57:00 +0300

    Added reflective parameters parsing

--- components/Router.php
+++ components/Router.php
@@ -51,9 +51,53 @@ final class Router
             throw new \Exception($message);
         }
 
-        return call_user_func_array(
-            [$controllerObject, $this->dispatcher->getAction()],
+        $params = $this->getArgumentsArray(
+            $controllerObject,
+            $this->dispatcher->getAction(),
             $this->dispatcher->getParams()
         );
+
+        return call_user_func_array([$controllerObject, $this->dispatcher->getAction()], $params);
+    }
+
+    /**
+     * @param Controller $controller
+     * @param string $action
+     * @param array $params
+     * @return array
+     * @throws \Exception
+     */
+    private function getArgumentsArray(Controller $controller, $action, array $params = [])
+    {
+        $reflectionController = new \ReflectionObject($controller);
+        $reflectionAction = $reflectionController->getMethod($action);
+
+        $requiredParams = [];
+        $additionalParams = [];
+
+        $requiredParamsQuantity = $reflectionAction->getNumberOfRequiredParameters();
+        foreach ($reflectionAction->getParameters() as $index => $param) {
+            if ($index < $requiredParamsQuantity) {
+                $requiredParams[] = $param->getName();
+            } else {
+                $additionalParams[$param->getName()] = $param->getDefaultValue();
+            }
+        }
+
+        $result = [];
+
+        foreach ($requiredParams as $requiredParam) {
+            if (array_key_exists($requiredParam, $params)) {
+                $result[$requiredParam] = $params[$requiredParam];
+            } else {
+                throw new \Exception("Required parameter '{$requiredParam}' is not transferred");
+            }
+        }
+
+        foreach ($additionalParams as $key => $defaultValue) {
+            $result[$key] = array_key_exists($key, $params) ? $params[$key] : $defaultValue;
+        }
+
+        return $result;
     }
 }
